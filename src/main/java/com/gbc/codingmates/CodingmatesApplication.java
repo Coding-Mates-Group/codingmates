3b09be93a20ef04f855107677c23c10d8c39d32c
Brian <brianlee6484@gmail.com>
2022-08-22 14:33:59 +0900

    Initial commit

---
+++ src/main/java/com/gbc/codingmates/CodingmatesApplication.java
@@ -0,0 +1,13 @@
+package com.gbc.codingmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingmatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbc/codingmates/CodingmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbc.codingmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
