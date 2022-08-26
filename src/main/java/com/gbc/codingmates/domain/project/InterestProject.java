9488670b548fa5cfc800a93f3b85b7f5d1143c33
jhsong2580 <thdwmdgns@naver.com>
2022-08-26 22:55:53 +0900

    feat: InterestProject entity

---
+++ src/main/java/com/gbc/codingmates/domain/project/InterestProject.java
@@ -0,0 +1,45 @@
+package com.gbc.codingmates.domain.project;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.gbc.codingmates.domain.member.Member;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public class InterestProject {
+
+    @Id
+    @Column(name = "INTEREST_PROJECT_ID")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "project_id", foreignKey = @ForeignKey(name = "FK_InterestProject_Project"))
+    private Project project;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "FK_InterestProject_Member"))
+    private Member member;
+
+    private Boolean acceptInfo;
+
+    @Builder
+    public InterestProject(Project project, Member member, Boolean acceptInfo) {
+        this.project = project;
+        this.member = member;
+        this.acceptInfo = acceptInfo;
+    }
+}
