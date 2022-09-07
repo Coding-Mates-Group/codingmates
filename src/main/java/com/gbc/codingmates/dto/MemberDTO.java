f4a54877c89be650a6020477fec7463545c1f83d
jhsong2580 <thdwmdgns@naver.com>
2022-09-07 23:24:28 +0900

    feat: member -> memberDTO factory method

--- src/main/java/com/gbc/codingmates/dto/MemberDTO.java
+++ src/main/java/com/gbc/codingmates/dto/MemberDTO.java
@@ -1,20 +1,28 @@
 package com.gbc.codingmates.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.gbc.codingmates.domain.member.Member;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-
-import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 @Builder
 public class MemberDTO {
+
     @NotNull
     private String username;
 
     @NotNull
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    public static MemberDTO from(Member member) {
+        return MemberDTO.builder()
+            .username(member.getEmail())
+            .password(member.getPassword())
+            .build();
+    }
 }
