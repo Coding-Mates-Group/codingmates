9668d976c19d07b4c1ce4a0e99f04b2bebdfaba5
jhsong2580 <thdwmdgns@naver.com>
2022-09-07 23:27:04 +0900

    feat: google oAuth 요청 및 필요 정보 획득을 위한 Resttemplate

---
+++ src/main/java/com/gbc/codingmates/config/oAuth/GoogleOauthRestTemplate.java
@@ -0,0 +1,64 @@
+package com.gbc.codingmates.config.oAuth;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GoogleOauthRestTemplate {
+
+    @Value("${spring.security.oauth2.client.registration.google.auth-code-url}")
+    private String googleAuthCodeURL;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleClientSecret;
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+    @Value("${spring.security.oauth2.client.registration.google.redirect-url}")
+    private String googleRedirectURL;
+
+    @Value("${spring.security.oauth2.client.registration.google.userinfo-url}")
+    private String googleUserInfoURL;
+
+    public String getAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> params = new HashMap<>();
+        params.put("grant_type", "authorization_code");
+        params.put("client_id", googleClientId);
+        params.put("client_secret", googleClientSecret);
+        params.put("redirect_uri", googleRedirectURL);
+        params.put("code", code);
+
+        ResponseEntity<Map> response = restTemplate.postForEntity(googleAuthCodeURL,
+            params, Map.class);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new IllegalArgumentException();
+        }
+
+        return (String) response.getBody().get("access_token");
+    }
+
+    public String getEmailByAccessToken(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + accessToken);
+
+        ResponseEntity<Map> response = restTemplate.exchange(googleUserInfoURL, HttpMethod.GET,
+            new HttpEntity<>("", httpHeaders),
+            Map.class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new IllegalArgumentException();
+        }
+
+        return (String)response.getBody().get("email");
+    }
+
+}
