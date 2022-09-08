be4b1dbab1bd8a52c53c1aeb19c07cb16c9e3c25
jhsong2580 <thdwmdgns@naver.com>
2022-09-08 13:13:55 +0900

    feat: OAuth entity

---
+++ src/main/java/com/gbc/codingmates/domain/member/OAuth.java
@@ -0,0 +1,39 @@
+package com.gbc.codingmates.domain.member;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.gbc.codingmates.domain.BaseTimeEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class OAuth extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "oauth_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(STRING)
+    private OAuthType provider;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", foreignKey = @ForeignKey(name = "fk_oauth_member"))
+    private Member member;
+}
