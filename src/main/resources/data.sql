82d07e019afffd5e90f33301f56dfb43cd4bbaba
jhsong2580 <thdwmdgns@naver.com>
2022-09-08 13:17:08 +0900

    sql: member entity 구조 변경 및 oauth entity 추가로 쿼리 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into member values (default, now(), now(), 'https://github.com/jhsong2580', null, 'VIP', 'sjh');
+insert into OAUTH values(default, now(), now(), 'thdwmdgns1@gmail.com', 'GOOGLE', 1L);
