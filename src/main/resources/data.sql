a089e378b9c526d76470b963f11d8bc63af02d52
jhsong2580 <thdwmdgns@naver.com>
2022-09-14 22:42:10 +0900

    sql: github oAuth 정보 쿼리 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into member values (default, now(), now(), 'https://github.com/jhsong2580', null, 'VIP', 'sjh');
 insert into OAUTH values(default, now(), now(), 'thdwmdgns1@gmail.com', 'GOOGLE', 1L);
+insert into OAUTH values(default, now(), now(), 'thdwmdgns@naver.com', 'GITHUB', 1L);
