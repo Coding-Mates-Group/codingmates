555a4a5e19067470d512d241946a195779c31353
jhsong2580 <thdwmdgns@naver.com>
2022-09-17 14:14:48 +0900

    fix: jwt Token에 해당 사용자 id 값 추가

--- src/main/java/com/gbc/codingmates/jwt/TokenProvider.java
+++ src/main/java/com/gbc/codingmates/jwt/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.gbc.codingmates.jwt;
 
+import com.gbc.codingmates.config.UserAuthentication;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -26,6 +27,7 @@ public class TokenProvider implements InitializingBean {
 
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
+    private static final String MEMBER_ID = "memberId";
     private final String secret;
     private final long tokenValidityInMilliseconds;
     private Key key;
@@ -45,7 +47,7 @@ public class TokenProvider implements InitializingBean {
     }
 
     //create token using Authentication object's authority info
-    public String createToken(Authentication authentication) {
+    public String createToken(UserAuthentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
@@ -56,6 +58,7 @@ public class TokenProvider implements InitializingBean {
         return Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
+                .claim(MEMBER_ID, authentication.getMemberId())
                 .signWith(key, SignatureAlgorithm.HS256)
                 .setExpiration(validity)
                 .compact();
@@ -81,6 +84,15 @@ public class TokenProvider implements InitializingBean {
         return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
+    public Long getMemberIdByToken(String token){
+        Claims claims = Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+        return (Long)claims.get(MEMBER_ID);
+    }
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
