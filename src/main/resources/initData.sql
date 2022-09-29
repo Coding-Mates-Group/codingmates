7dae748f97aff23189ba8cd9ceb1e9c56faaa751
jhsong2580 <thdwmdgns@naver.com>
2022-09-29 23:29:38 +0900

    sql: skill들 추가

--- src/main/resources/initData.sql
+++ src/main/resources/initData.sql
@@ -1,14 +1,18 @@
-
-
-
-
-
-
-
-
-
-insert into member values (default, now(), now(), 'thdwmdgns@naver.com','password','www.github.com/jhsong2580',null, 'VIP', 'sjh');
--- insert into OAUTH values(default, now(), now(), '111599421069007040504', 'GOOGLE', 1L);
-insert into OAUTH values(default, now(), now(), '19621155', 'GITHUB', 1L);
+insert into member values (default, now(), now(), 'thdwmdgns@naver.com', null,'VIP', null,'https://github.com/jhsong2580',null, 'sjh');
+insert into OAUTH values(default, now(), now(), '111599421069007040504', 'GOOGLE', 1L);
+-- insert into OAUTH values(default, now(), now(), '19621155', 'GITHUB', 1L);
+insert into OAUTH values(default, now(), now(), '5336916936425567', 'FACEBOOK', 1L);
 insert into member (username, password) values ('brian', '$2a$10$KSh3zFNfCSfOJ2a4g8bPVenlbCgmYyG6MXwdBRVam8Cl6BFZJDT9a');
+insert into skill values(default, 'SPRING');
+insert into skill values(default, 'JPA');
+insert into skill values(default, 'PYTHON');
 -- 123456
+
+
+
+
+
+
+
+
+
