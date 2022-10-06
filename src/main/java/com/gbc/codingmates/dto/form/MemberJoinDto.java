b2dde3f35cd0bcf0413847f79db66b2a12328793
jhsong2580 <thdwmdgns@naver.com>
2022-10-06 23:04:58 +0900

    feat: 회원가입 검증 정보 추가

--- src/main/java/com/gbc/codingmates/dto/form/MemberJoinDto.java
+++ src/main/java/com/gbc/codingmates/dto/form/MemberJoinDto.java
@@ -3,12 +3,12 @@ package com.gbc.codingmates.dto.form;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.multipart.MultipartFile;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,7 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class MemberJoinDto {
 
     @NotEmpty(message = "user alias must not empty")
+    @Length(min = 4, max = 20, message = "user alias length must be 4 ~ 20 words")
+    @Pattern(regexp = "^[a-z0-9]*", message = "user alias can be made by numbers and lower spelling")
     private String userAlias;
+
     @NotEmpty(message = "token must not empty")
     private String token;
     private List<Long> skillIds = new ArrayList<>();
---
+++ src/test/java/com/gbc/codingmates/dto/form/MemberJoinDtoTest.java
@@ -0,0 +1,42 @@
+package com.gbc.codingmates.dto.form;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class MemberJoinDtoTest {
+
+    ValidatorFactory validatorFactory;
+    Validator validator;
+
+    @BeforeEach
+    public void init() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    public void exit() {
+        validatorFactory.close();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"aa:asdf:false", "Aaaa:asdf:false", "A aa:asdf:false", "a!aa:asdf:false",
+        "aaaa:asdf:true"}, delimiter = ':')
+    public void memberJoinDTOValidateTest(String userAlias, String token, boolean isOk) {
+        //given
+        MemberJoinDto memberJoinDto = new MemberJoinDto(userAlias, token, null);
+
+        //when
+        Set<ConstraintViolation<MemberJoinDto>> validatedResult = validator.validate(memberJoinDto);
+
+        //then
+        assertThat(validatedResult.size() == 0).isEqualTo(isOk);
+    }
+}
