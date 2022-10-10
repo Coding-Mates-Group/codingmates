891d1b3585f99a11c00d9b86874739afaa72d25e
jhsong2580 <thdwmdgns@naver.com>
2022-10-10 19:51:19 +0900

    fix: RequestBody로 변경

--- module-member/src/main/java/com/gbc/codingmates/controller/member/MemberController.java
+++ module-member/src/main/java/com/gbc/codingmates/controller/member/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -30,7 +31,7 @@ public class MemberController {
 
 
     @PostMapping(value = "", consumes = {APPLICATION_JSON_VALUE})
-    public ResponseEntity register(@RequestPart @Validated MemberJoinDto request,
+    public ResponseEntity register(@RequestBody @Validated MemberJoinDto request,
         BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.getFieldErrors().toString());
