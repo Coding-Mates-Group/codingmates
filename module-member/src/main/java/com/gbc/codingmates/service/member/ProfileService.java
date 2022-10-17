078eec7b4588af83df8f50a1514dbec277b02c44
jhsong2580 <thdwmdgns@naver.com>
2022-10-17 22:41:04 +0900

    feat: profile update시 member profile path변경 추가

--- module-member/src/main/java/com/gbc/codingmates/service/member/ProfileService.java
+++ module-member/src/main/java/com/gbc/codingmates/service/member/ProfileService.java
@@ -1,22 +1,37 @@
 package com.gbc.codingmates.service.member;
 
+import com.gbc.codingmates.domain.member.Member;
+import com.gbc.codingmates.domain.member.MemberRepository;
 import com.gbc.codingmates.utils.FileHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProfileService {
 
     private final FileHandler fileHandler;
+    private final MemberRepository memberRepository;
 
+    @Transactional
     public ResponseEntity saveProfile(MultipartFile profile, Long memberId, Long jwtMemberId) {
         if (memberId != jwtMemberId) {
             throw new IllegalArgumentException("invalid access for update profile");
         }
-        fileHandler.saveProfileImage(profile, memberId);
+
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(
+                () -> new IllegalArgumentException(String.format("member not exist %s", memberId))
+            );
+
+        String path = fileHandler.saveProfileImage(profile, memberId);
+
+        member.mapMemberProfileImagePath(path);
+
         return ResponseEntity.ok().build();
     }
 
