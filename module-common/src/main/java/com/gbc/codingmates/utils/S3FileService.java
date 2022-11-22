d3068f9d4ff60128135f7d2d0428a50f5a45735f
jhsong2580 <thdwmdgns@naver.com>
2022-11-22 22:30:42 +0900

    feat: S3 file Save event 생성

--- module-common/src/main/java/com/gbc/codingmates/utils/S3FileService.java
+++ module-common/src/main/java/com/gbc/codingmates/utils/S3FileService.java
@@ -4,17 +4,17 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.gbc.codingmates.dto.member.MemberDto;
+import java.io.IOException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.UUID;
-
 @RequiredArgsConstructor
 @Service
 public class S3FileService {
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
@@ -34,7 +34,21 @@ public class S3FileService {
         return amazonS3.getUrl(bucket, s3FileName).toString();
     }
 
-    public void deleteFile(String fileName){
+    public String upload(MultipartFile multipartFile, String fileAlias) throws IOException {
+        //for saved file in s3 to have unique names via UUID.randomUUID()
+        String s3FileName = UUID.randomUUID() + "-" + fileAlias;
+
+        //inform file's size via ContentLength to S3
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentLength(multipartFile.getInputStream().available());
+
+        //s3 api method to open file stream and upload it to S3
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+
+        return amazonS3.getUrl(bucket, s3FileName).toString();
+    }
+
+    public void deleteFile(String fileName) {
         amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
     }
 }
